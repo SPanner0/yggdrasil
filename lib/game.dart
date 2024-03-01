a2bc080f259a11681a5fc1e517cd00d2f10b726b
Wolfus20 <shawnpandev@gmail.com>
2024-03-01 12:26:49 +0800

    Add minimal game page

--- lib/game.dart
+++ lib/game.dart
@@ -1,67 +1,75 @@
 import 'package:flutter/material.dart';
+import 'plants.dart';
 
-class GamePage extends StatelessWidget {
-    const GamePage({super.key});
+class GamePage extends StatefulWidget {
+  const GamePage({super.key});
 
-    @override
-        Widget build(BuildContext context) {
-            return  Scaffold(
-                    appBar: AppBar(
-                        title: const Text("Yggdrasil")),
-                        body: Column(
-                            children: [
-                                Container(height: 48.0,),
-                                Container(
-                                    child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                        children: [
-                                            Container(
-                                                child: const Column(
-                                                    children: [
-                                                        Text("Plant 1"),
-                                                        Text("0")
-                                                    ]
-                                                )
-                                            ),
-                                            Container(
-                                                child: const Column(
-                                                    children: [
-                                                        Text("Plant 2"),
-                                                        Text("0")
-                                                    ]
-                                                )
-                                            )
-                                        ]
-                                    )
-                                )
-                            ]
-                        ));
-        }
+  @override
+  State<StatefulWidget> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+    RestorableInt coins = RestorableInt(0);
+    RestorableInt day = RestorableInt(0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Yggdrasil")),
+        body: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          return Column(children: [
+            SizedBox(
+              width: constraints.maxWidth,
+              height: constraints.maxHeight * 0.6,
+              child: Image.asset("assets/images/window.jpg"),
+            ),
+            Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(image: AssetImage("assets/images/table.jpg"), fit: BoxFit.fill),
+                    ),
+                child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                  Stack(children: [
+                    PlantBox(),
+                    Pot(),
+                  ]),
+                  Stack(children: [
+                    PlantBox(),
+                    Pot(),
+                  ]),
+                  Stack(children: [
+                    PlantBox(),
+                    Pot(),
+                  ])
+                ]))
+          ]);
+        }));
+  }
 }
 
 class PlantBox extends StatefulWidget {
-    const PlantBox({super.key});
+  const PlantBox({super.key});
 
-    @override
-        State<PlantBox> createState() => _PlantBoxState();
-}
-
-enum PlantType {
-    cactus,
-    orchid
+  @override
+  State<PlantBox> createState() => _PlantBoxState();
 }
 
 class _PlantBoxState extends State<PlantBox> {
-    final PlantType plantType = PlantType.cactus;
+  final PlantType plantType = PlantType.none;
 
-    @override
-        Widget build(BuildContext context) {
-            return Container(
-                    child: Column(
-                        children: [
-                            Text(plantType.toString()),
-                        ]
-                    )
-            );
-        }
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(plantType.imagePath!);
+  }
+}
+
+class Pot extends StatelessWidget {
+  const Pot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("assets/images/pot.png");
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,89 +4,88 @@ import 'package:google_fonts/google_fonts.dart';
 import 'home.dart';
 
 void main() {
-    runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-    const MyApp({super.key});
+  const MyApp({super.key});
 
-    // This widget is the root of your application.
-    @override
-        Widget build(BuildContext context) {
-            return MaterialApp(
-                    title: 'Yggdrasil',
-                    theme: appTheme,
-                    home: const HomePage(),
-                    );
-        }
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yggdrasil',
+      theme: appTheme,
+      home: const HomePage(),
+    );
+  }
 }
 
-
 final ThemeData appTheme = ThemeData(
-        // primaryColor: const Color(0xFF7CB342), // Green
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF7CB342)),
-        // splashColor: const Color(0xFFF07167), // Light pink
-        // scaffoldBackgroundColor: const Color(0xFFF5F5F5), // Light gray
+  // primaryColor: const Color(0xFF7CB342), // Green
+  colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF7CB342)),
+  // splashColor: const Color(0xFFF07167), // Light pink
+  // scaffoldBackgroundColor: const Color(0xFFF5F5F5), // Light gray
 
-        textTheme: TextTheme(
-            headlineLarge: GoogleFonts.montserrat(
-                fontSize: 32.0,
-                color: const Color(0xFF333333), // Dark gray
-                ),
-            headlineMedium: GoogleFonts.montserrat(
-                fontSize: 24.0,
-                color: const Color(0xFF333333), // Dark gray
-                ),
-            headlineSmall: GoogleFonts.montserrat(
-                fontSize: 20.0,
-                color: const Color(0xFF333333), // Dark gray
-                ),
-            bodyLarge: GoogleFonts.openSans(
-                fontSize: 16.0,
-                color: const Color(0xFF333333), // Dark gray
-                ),
-            bodyMedium: GoogleFonts.openSans(
-                fontSize: 14.0,
-                color: const Color(0xFF333333), // Dark gray
-                ),
-            bodySmall: GoogleFonts.openSans(
-                    fontSize: 12.0,
-                    color: const Color(0xFF333333), // Dark gray
-                    ),
-            ),
+  textTheme: TextTheme(
+    headlineLarge: GoogleFonts.montserrat(
+      fontSize: 32.0,
+      color: const Color(0xFF333333), // Dark gray
+    ),
+    headlineMedium: GoogleFonts.montserrat(
+      fontSize: 24.0,
+      color: const Color(0xFF333333), // Dark gray
+    ),
+    headlineSmall: GoogleFonts.montserrat(
+      fontSize: 20.0,
+      color: const Color(0xFF333333), // Dark gray
+    ),
+    bodyLarge: GoogleFonts.openSans(
+      fontSize: 16.0,
+      color: const Color(0xFF333333), // Dark gray
+    ),
+    bodyMedium: GoogleFonts.openSans(
+      fontSize: 14.0,
+      color: const Color(0xFF333333), // Dark gray
+    ),
+    bodySmall: GoogleFonts.openSans(
+      fontSize: 12.0,
+      color: const Color(0xFF333333), // Dark gray
+    ),
+  ),
 
-            appBarTheme: AppBarTheme(
-                    backgroundColor: const Color(0xFF7CB342), // Green
-                    titleTextStyle: GoogleFonts.montserrat(
-                        fontSize: 20.0,
-                        color: Colors.white, // White
-                        ),
-                    ),
+  appBarTheme: AppBarTheme(
+    backgroundColor: const Color(0xFF7CB342), // Green
+    titleTextStyle: GoogleFonts.montserrat(
+      fontSize: 20.0,
+      color: Colors.white, // White
+    ),
+  ),
 
-            elevatedButtonTheme: ElevatedButtonThemeData(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFFF07167), // Light pink
-                        foregroundColor: Colors.white, // White
-                        textStyle: GoogleFonts.montserrat(
-                            color: Colors.white, // White
-                            ),
-                        ),
-                    ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: const Color(0xFFF07167), // Light pink
+      foregroundColor: Colors.white, // White
+      textStyle: GoogleFonts.montserrat(
+        color: Colors.white, // White
+      ),
+    ),
+  ),
 
-            outlinedButtonTheme: OutlinedButtonThemeData(
-                    style: OutlinedButton.styleFrom(
-                        backgroundColor: const Color(0xFF7CB342), // Green
-                        foregroundColor: Colors.white, // White
-                        textStyle: GoogleFonts.montserrat(
-                            color: const Color(0xFF7CB342), // Green
-                            ),
-                        ),
-                    ),
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      backgroundColor: const Color(0xFF7CB342), // Green
+      foregroundColor: Colors.white, // White
+      textStyle: GoogleFonts.montserrat(
+        color: const Color(0xFF7CB342), // Green
+      ),
+    ),
+  ),
 
-            inputDecorationTheme: const InputDecorationTheme(
-                    fillColor: Color(0xFFF5F5F5), // Light gray
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFF7CB342), width: 2.0), // Green
-                        ),
-                    ),
-            );
+  inputDecorationTheme: const InputDecorationTheme(
+    fillColor: Color(0xFFF5F5F5), // Light gray
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Color(0xFF7CB342), width: 2.0), // Green
+    ),
+  ),
+);
